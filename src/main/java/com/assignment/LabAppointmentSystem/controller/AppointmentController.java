b1eb745a92e3b123b10af26d4341d49b0a5a8c21
Jineth Perera <jinethperera889gmail.com>
2024-03-09 07:41:01 +0530

    patient appointment view

--- src/main/java/com/assignment/LabAppointmentSystem/controller/AppointmentController.java
+++ src/main/java/com/assignment/LabAppointmentSystem/controller/AppointmentController.java
@@ -3,6 +3,7 @@ package com.assignment.LabAppointmentSystem.controller;
 import com.assignment.LabAppointmentSystem.model.Appointment;
 import com.assignment.LabAppointmentSystem.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/appointment")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3001")
 public class AppointmentController {
     @Autowired
     private AppointmentService appointmentService;
@@ -22,28 +23,10 @@ public class AppointmentController {
         return ResponseEntity.ok(bookedAppointment);
     }
 
-    @DeleteMapping("/cancel/{appointmentId}")
-    public ResponseEntity<Void> cancelAppointment(@PathVariable Long appointmentId) {
-        appointmentService.cancelAppointment(appointmentId);
-        return ResponseEntity.noContent().build();
-    }
-
-    @PutMapping("/reschedule/{appointmentId}")
-    public ResponseEntity<Void> rescheduleAppointment(@PathVariable Long appointmentId, @RequestParam LocalDateTime newDateTime) {
-        appointmentService.rescheduleAppointment(appointmentId, newDateTime);
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/patient/{patientId}")
-    public ResponseEntity<List<Appointment>> getAppointmentsByPatientId(@PathVariable Long patientId) {
-        List<Appointment> appointments = appointmentService.getAppointmentsByPatientId(patientId);
-        return ResponseEntity.ok(appointments);
-    }
-
-    @GetMapping("/status/{status}")
-    public ResponseEntity<List<Appointment>> getAppointmentsByStatus(@PathVariable String status) {
-        List<Appointment> appointments = appointmentService.getAppointmentsByStatus(status);
-        return ResponseEntity.ok(appointments);
+    @GetMapping("/appointments")
+    public ResponseEntity<List<Appointment>> getMyAppointments() {
+        List<Appointment> appointments = appointmentService.getAllAppointments();
+        return new ResponseEntity<>(appointments, HttpStatus.OK);
     }
 
 
--- src/main/java/com/assignment/LabAppointmentSystem/service/AppointmentService.java
+++ src/main/java/com/assignment/LabAppointmentSystem/service/AppointmentService.java
@@ -1,6 +1,7 @@
 package com.assignment.LabAppointmentSystem.service;
 
 import com.assignment.LabAppointmentSystem.model.Appointment;
+import com.assignment.LabAppointmentSystem.model.Doctor;
 import com.assignment.LabAppointmentSystem.repository.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,31 +24,10 @@ public class AppointmentService {
     public Appointment bookAppointment(Appointment appointment) {
         return appointmentRepository.save(appointment);
     }
-
-    public void cancelAppointment(Long appointmentId) {
-        Optional<Appointment> optionalAppointment = appointmentRepository.findById(appointmentId);
-        optionalAppointment.ifPresent(appointment -> {
-            appointment.setStatus("Canceled");
-            appointmentRepository.save(appointment);
-        });
+    public List<Appointment> getAllAppointments() {
+        return appointmentRepository.findAll();
     }
 
-    public void rescheduleAppointment(Long appointmentId, LocalDateTime newDateTime) {
-        Optional<Appointment> optionalAppointment = appointmentRepository.findById(appointmentId);
-        optionalAppointment.ifPresent(appointment -> {
-            appointment.setAppointmentDateTime(newDateTime);
-            appointmentRepository.save(appointment);
-        });
-    }
-
-    public List<Appointment> getAppointmentsByPatientId(Long patientId) {
-        // Implement logic to fetch appointments by patient ID
-        return appointmentRepository.findByPatientId(patientId);
-    }
-
-    public List<Appointment> getAppointmentsByStatus(String status) {
-        return appointmentRepository.findByStatusAndAppointmentDateTimeAfter(status, LocalDateTime.now());
-    }
 
 
 }
